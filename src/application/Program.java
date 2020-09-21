649351f1ffea6c6e16b981e463dfb5ca38302ecb
Yuri Baldan <yuribbp@gmail.com>
2020-09-21 12:13:22 -0300

    Mensagem de Boa noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,9 @@ package application;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		System.out.println("Bom dia!");
 		System.out.println("Boa tarde!");
+		System.out.println("Boa noite!");
 	}
-
 }
